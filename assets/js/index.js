3500b7e11d33fdaf2e33ab677787d950b710b325
Xander Dumaine <xander.dumaine@inin.com>
2016-11-29 12:32:55 -0500

    rip out everything I don't need

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,5 @@
 /*
 ===============================================================================
-  Subbscribe
-===============================================================================
-*/
-$('body').subbscribe({
-    list: "MailChimp",
-    url: "//1bytebeta.us9.list-manage.com/subscribe/post?u=1c261e60d8259c0c636801494&id=7fa99bf359",
-    name: "<a href='https://twitter.com/shlominissan' target='_blank'>@shlominissan</a>",
-    thumbnail: "https://s3-ap-southeast-2.amazonaws.com/subbscribe/img/shlomi.jpg",
-    color: "#F2545B",
-    delay: 3,
-});
-/*
-===============================================================================
   Infinity Scroll - YEAH!
 ===============================================================================
 */
@@ -23,17 +10,3 @@ $('body').subbscribe({
         dataType: 'html',
         maxPage: 3,
     });
-/*
-===============================================================================
-  Disqus 
-===============================================================================
-*/
-(function () { // DON'T EDIT BELOW THIS LINE
-    var d = document,
-        s = d.createElement('script');
-
-    s.src = '//tilligettherecom.disqus.com/embed.js';
-
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-})();
